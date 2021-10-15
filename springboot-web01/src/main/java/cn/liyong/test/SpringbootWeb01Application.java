963cbaa788eb247614ada0f8ef6f0a04ee79540c
xiaosi2 <xiaosi2@douyu.tv>
2021-10-15 17:48:26 +0800

    [BGF] 消除一些代码异味.

--- springboot-web01/src/main/java/cn/liyong/test/SpringbootWeb01Application.java
+++ springboot-web01/src/main/java/cn/liyong/test/SpringbootWeb01Application.java
@@ -1,7 +1,6 @@
 package cn.liyong.test;
 
 import cn.liyong.test.springboot.entity.sys.UserTest;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
